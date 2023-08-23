1a6373311c051233ec034798cce041e3d6da0bf3
avendanio2000 <106839424+avendanio2000@users.noreply.github.com>
2023-08-23 12:00:39 -0300

    Agregar archivos de proyecto.

---
+++ NetCoreYouTube/Models/Jwt.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Claims;
+
+namespace NetCoreYouTube.Models
+{
+    public class Jwt
+    {
+        public string Key { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string Subject { get; set; }
+
+        public static dynamic validarToken (ClaimsIdentity identity)
+        {
+            try
+            {
+                if (identity.Claims.Count() ==0)
+                {
+                    return new
+                    {
+                        success = false,
+                        message = "Verificar si estas enviando un token valido",
+                        result = ""
+                    };
+                }
+
+                var id = identity.Claims.FirstOrDefault(x => x.Type == "Id").Value;
+
+                Usuario usuario = Usuario.DB().FirstOrDefault(x => x.idUsuario == id);
+
+                return new
+                {
+                    success = true,
+                    message = "exito",
+                    result = usuario
+                };
+            }
+            catch (Exception ex)
+            {
+                return new
+                {
+                    success = false,
+                    message = "Catch: " + ex.Message,
+                    result = ""
+                };
+            }
+
+            return null;
+        }       
+
+    }
+}
